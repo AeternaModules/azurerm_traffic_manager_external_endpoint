3e2d2072ff054fc7eaba9e2af8c967e16dc5951c
Daniel Allen <dan@aeternagroup.net>
2026-01-11 22:15:48 +0000

    Update to schema version v4.57.0

---
+++ variables.tf
@@ -0,0 +1,45 @@
+variable "traffic_manager_external_endpoints" {
+  description = <<EOT
+Map of traffic_manager_external_endpoints, attributes below
+Required:
+    - name
+    - profile_id
+    - target
+Optional:
+    - always_serve_enabled
+    - enabled
+    - endpoint_location
+    - geo_mappings
+    - priority
+    - weight
+    - custom_header (block):
+        - name (required)
+        - value (required)
+    - subnet (block):
+        - first (required)
+        - last (optional)
+        - scope (optional)
+EOT
+
+  type = map(object({
+    name                 = string
+    profile_id           = string
+    target               = string
+    always_serve_enabled = optional(bool, false)
+    enabled              = optional(bool, true)
+    endpoint_location    = optional(string)
+    geo_mappings         = optional(list(string))
+    priority             = optional(number)
+    weight               = optional(number, 1)
+    custom_header = optional(object({
+      name  = string
+      value = string
+    }))
+    subnet = optional(object({
+      first = string
+      last  = optional(string)
+      scope = optional(number)
+    }))
+  }))
+}
+
