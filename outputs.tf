ddab175fcd6d8f357626316e405782497141b17b
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:21:15 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,49 +1,45 @@
-output "traffic_manager_external_endpoints" {
-  description = "All traffic_manager_external_endpoint resources"
-  value       = azurerm_traffic_manager_external_endpoint.traffic_manager_external_endpoints
-}
 output "traffic_manager_external_endpoints_always_serve_enabled" {
-  description = "List of always_serve_enabled values across all traffic_manager_external_endpoints"
-  value       = [for k, v in azurerm_traffic_manager_external_endpoint.traffic_manager_external_endpoints : v.always_serve_enabled]
+  description = "Map of always_serve_enabled values across all traffic_manager_external_endpoints, keyed the same as var.traffic_manager_external_endpoints"
+  value       = { for k, v in azurerm_traffic_manager_external_endpoint.traffic_manager_external_endpoints : k => v.always_serve_enabled }
 }
 output "traffic_manager_external_endpoints_custom_header" {
-  description = "List of custom_header values across all traffic_manager_external_endpoints"
-  value       = [for k, v in azurerm_traffic_manager_external_endpoint.traffic_manager_external_endpoints : v.custom_header]
+  description = "Map of custom_header values across all traffic_manager_external_endpoints, keyed the same as var.traffic_manager_external_endpoints"
+  value       = { for k, v in azurerm_traffic_manager_external_endpoint.traffic_manager_external_endpoints : k => v.custom_header }
 }
 output "traffic_manager_external_endpoints_enabled" {
-  description = "List of enabled values across all traffic_manager_external_endpoints"
-  value       = [for k, v in azurerm_traffic_manager_external_endpoint.traffic_manager_external_endpoints : v.enabled]
+  description = "Map of enabled values across all traffic_manager_external_endpoints, keyed the same as var.traffic_manager_external_endpoints"
+  value       = { for k, v in azurerm_traffic_manager_external_endpoint.traffic_manager_external_endpoints : k => v.enabled }
 }
 output "traffic_manager_external_endpoints_endpoint_location" {
-  description = "List of endpoint_location values across all traffic_manager_external_endpoints"
-  value       = [for k, v in azurerm_traffic_manager_external_endpoint.traffic_manager_external_endpoints : v.endpoint_location]
+  description = "Map of endpoint_location values across all traffic_manager_external_endpoints, keyed the same as var.traffic_manager_external_endpoints"
+  value       = { for k, v in azurerm_traffic_manager_external_endpoint.traffic_manager_external_endpoints : k => v.endpoint_location }
 }
 output "traffic_manager_external_endpoints_geo_mappings" {
-  description = "List of geo_mappings values across all traffic_manager_external_endpoints"
-  value       = [for k, v in azurerm_traffic_manager_external_endpoint.traffic_manager_external_endpoints : v.geo_mappings]
+  description = "Map of geo_mappings values across all traffic_manager_external_endpoints, keyed the same as var.traffic_manager_external_endpoints"
+  value       = { for k, v in azurerm_traffic_manager_external_endpoint.traffic_manager_external_endpoints : k => v.geo_mappings }
 }
 output "traffic_manager_external_endpoints_name" {
-  description = "List of name values across all traffic_manager_external_endpoints"
-  value       = [for k, v in azurerm_traffic_manager_external_endpoint.traffic_manager_external_endpoints : v.name]
+  description = "Map of name values across all traffic_manager_external_endpoints, keyed the same as var.traffic_manager_external_endpoints"
+  value       = { for k, v in azurerm_traffic_manager_external_endpoint.traffic_manager_external_endpoints : k => v.name }
 }
 output "traffic_manager_external_endpoints_priority" {
-  description = "List of priority values across all traffic_manager_external_endpoints"
-  value       = [for k, v in azurerm_traffic_manager_external_endpoint.traffic_manager_external_endpoints : v.priority]
+  description = "Map of priority values across all traffic_manager_external_endpoints, keyed the same as var.traffic_manager_external_endpoints"
+  value       = { for k, v in azurerm_traffic_manager_external_endpoint.traffic_manager_external_endpoints : k => v.priority }
 }
 output "traffic_manager_external_endpoints_profile_id" {
-  description = "List of profile_id values across all traffic_manager_external_endpoints"
-  value       = [for k, v in azurerm_traffic_manager_external_endpoint.traffic_manager_external_endpoints : v.profile_id]
+  description = "Map of profile_id values across all traffic_manager_external_endpoints, keyed the same as var.traffic_manager_external_endpoints"
+  value       = { for k, v in azurerm_traffic_manager_external_endpoint.traffic_manager_external_endpoints : k => v.profile_id }
 }
 output "traffic_manager_external_endpoints_subnet" {
-  description = "List of subnet values across all traffic_manager_external_endpoints"
-  value       = [for k, v in azurerm_traffic_manager_external_endpoint.traffic_manager_external_endpoints : v.subnet]
+  description = "Map of subnet values across all traffic_manager_external_endpoints, keyed the same as var.traffic_manager_external_endpoints"
+  value       = { for k, v in azurerm_traffic_manager_external_endpoint.traffic_manager_external_endpoints : k => v.subnet }
 }
 output "traffic_manager_external_endpoints_target" {
-  description = "List of target values across all traffic_manager_external_endpoints"
-  value       = [for k, v in azurerm_traffic_manager_external_endpoint.traffic_manager_external_endpoints : v.target]
+  description = "Map of target values across all traffic_manager_external_endpoints, keyed the same as var.traffic_manager_external_endpoints"
+  value       = { for k, v in azurerm_traffic_manager_external_endpoint.traffic_manager_external_endpoints : k => v.target }
 }
 output "traffic_manager_external_endpoints_weight" {
-  description = "List of weight values across all traffic_manager_external_endpoints"
-  value       = [for k, v in azurerm_traffic_manager_external_endpoint.traffic_manager_external_endpoints : v.weight]
+  description = "Map of weight values across all traffic_manager_external_endpoints, keyed the same as var.traffic_manager_external_endpoints"
+  value       = { for k, v in azurerm_traffic_manager_external_endpoint.traffic_manager_external_endpoints : k => v.weight }
 }
 
